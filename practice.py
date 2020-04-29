2d5c861d6ad646cb0ec94510b412176982f58161
deanbest <deanbest5320@gmail.com>
2020-04-29 13:06:19 +0800

    add single show

--- practice.py
+++ practice.py
@@ -10,5 +10,7 @@ while True:
 	#理解的寫法 p.append(price) 
 	#簡潔寫法 p = [name, price, place] 
 	products.append([name, price, place])  #把小火車裝進大火車products清單中,更簡寫法
-
 print(products)
+
+for p in products: #印出清單中的每個東西
+	print(p[0], '是來自', p[2]) #印出每一個小清單的第0格及價格
